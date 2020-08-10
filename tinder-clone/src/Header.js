99bbe3c9dadb06012f639eacd2967f3510942dca
Jessica Huynh <huynhjessica19@gmail.com>
2020-08-09 18:03:14 -0700

    giving the icon classes

--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -2,13 +2,14 @@ import React from 'react';
 import "./Header.css";
 import PersonIcon from '@material-ui/icons/Person';
 import ForumIcon from '@material-ui/icons/Forum';
+import WhatshotIcon from '@material-ui/icons/Whatshot';
 
 function Header() {
     return (
         <div className="header">
-            <PersonIcon />
-            <h2>I am a header</h2>
-            <ForumIcon />
+            <PersonIcon className="header_icon" fontSize="large" />
+            <WhatshotIcon fontSize="large" />
+            <ForumIcon className="header_icon" fontSize="large" />
         </div>
     )
 }
