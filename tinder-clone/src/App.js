a47d75835669e258887173a914be81ced155ce2e
Jessica Huynh <huynhjessica19@gmail.com>
2020-08-09 19:45:36 -0700

    adding tinder cards

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import Header from './Header';
-import './App.css';
+import TinderCards from './TinderCards';
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
-     <h1>Let's build the tinder clone app</h1>
+      
+      <Header /> 
+      <Router>
+       
 
-      <Header />
+       <Switch>
+         <Route path="/chat">
+           <h1>I am the chat</h1>
+         </Route>
+         
+         <Route path="/">
+           <TinderCards />
+         </Route>
+       </Switch>
+
+
+
+
+
+
+      </Router>
 
     </div>
   );
 }
 
 export default App;
+  
--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -3,13 +3,20 @@ import "./Header.css";
 import PersonIcon from '@material-ui/icons/Person';
 import ForumIcon from '@material-ui/icons/Forum';
 import WhatshotIcon from '@material-ui/icons/Whatshot';
+import IconButton from '@material-ui/core/IconButton'
 
 function Header() {
     return (
         <div className="header">
+           <IconButton>
             <PersonIcon className="header_icon" fontSize="large" />
+           </IconButton>
+           
             <WhatshotIcon fontSize="large" />
+           
+           <IconButton>
             <ForumIcon className="header_icon" fontSize="large" />
+           </IconButton>
         </div>
     )
 }
