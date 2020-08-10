b64c88ca5c8c98e2a1e010eed1b169f5edc8f208
Jessica Huynh <huynhjessica19@gmail.com>
2020-08-09 20:33:11 -0700

    css for tinder cards

--- tinder-clone/src/TinderCards.js
+++ tinder-clone/src/TinderCards.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
 import TinderCard from 'react-tinder-card';
-
+import './TinderCards.css';
 
 function TinderCards() {
     const [people, setPeople] = useState([
         {
-            name: 'steve jobs',
-            url: ''
-        },
+            name: 'marshmello',
+            url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTEYcbhrtRoUbebD8t_10NYt3NkyoD4LJRwEQ&usqp=CAU'
+        },    
+
         {
-            name: 'mark zuckerberg',
-            url: ''
+            name: 'Dillion Francis',
+            url: 'https://d3vhc53cl8e8km.cloudfront.net/hello-staging/wp-content/uploads/2014/05/06235520/fNl7lJh0RhexB8Zf4uM9JWmZYhVdc9KTVwbDJtdy-972x597.jpeg'
         }
     ]);
     
@@ -19,9 +20,16 @@ function TinderCards() {
         <div>
             <h1>Tinder Cards</h1>
 
-            {people.map(person = (
-                <TinderCard>
-                    <div className="card">
+            {people.map((person) => (
+                <TinderCard
+                className="swipe"
+                key={person.name}
+                preventSwipe={['up', 'down']}
+                
+                >
+                   
+                    <div  style= {{backgroundImage: `url(${person.url})`}}    
+                    className="card">
                         <h3>{person.name}</h3>
                     </div>
                 </TinderCard>
