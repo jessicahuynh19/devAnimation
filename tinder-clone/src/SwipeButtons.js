884a1584971cd51cfd1d76b65def0dce66143c2f
Jessica Huynh <huynhjessica19@gmail.com>
2020-08-14 15:43:40 -0700

    adding buttons

--- tinder-clone/src/SwipeButtons.js
+++ tinder-clone/src/SwipeButtons.js
@@ -1,15 +1,34 @@
 import React from 'react';
-import ReplayIcon from 'material-ui/icons/Replay';
-import CloseIcon from 'material-ui/icons/Close';
-import StarRateIcon from 'material-ui/icons/StarRate';
-import FavoriteIcon from 'material-ui/icons/Favorite';
-import FlashOnIcon from 'material-ui/icons/FlashOn';
+import ReplayIcon from '@material-ui/icons/Replay';
+import CloseIcon from '@material-ui/icons/Close';
+import StarIcon from '@material-ui/icons/Star';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FlashOnIcon from '@material-ui/icons/FlashOn';
+import IconButton from '@material-ui/core/IconButton';
 import './SwipeButtons.css';
 
 const SwipeButtons = () => {
     return (
         <div className="swipeButtons">
-            <h1>Buttons goes here</h1>
+            <IconButton className="swipeButtons_repeat">
+            <ReplayIcon fontSize="large" />
+            </IconButton>
+            
+            <IconButton className="swipeButtons_left">
+            <CloseIcon fontSize="large" />
+            </IconButton>
+            
+            <IconButton className="swipeButtons_star">
+            <StarIcon fontSize="large" />
+            </IconButton>
+            
+            <IconButton className="swipButtons_right">
+            <FavoriteIcon fontSize="large" />
+            </IconButton>
+            
+            <IconButton  className="swipeButtons_lightning">
+            <FlashOnIcon fontSize="large" />
+            </IconButton>
         </div>
     )
 }
