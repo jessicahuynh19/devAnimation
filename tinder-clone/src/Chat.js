669f6f2fcd3d279a3d7f3594b235f17e54b2ed3a
Jessica Huynh <huynhjessica19@gmail.com>
2020-08-15 21:20:16 -0700

    chat styling

--- tinder-clone/src/Chat.js
+++ tinder-clone/src/Chat.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import './Chat.css';
 import Avatar from '@material-ui/core/Avatar';
 
 function Chat({ name, message, profilePic, timeStamp }) {
     return (
         <div className="chat">
-            <Avatar className="chat_image" alt= {name} src= {profilePic} />
+            <Avatar className="chat_image" src= {profilePic} />
+            <div className="chat_details">
+                <h2>{name}</h2>
+                <p>{message}</p>
+            </div>
+            <p className="chat_timestamp">{timeStamp}</p>
 
         </div>
-    )
+    );
 }
 
 export default Chat;
